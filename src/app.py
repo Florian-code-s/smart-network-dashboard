a7ddce39e4d9c725e02aff798751f816592f7dfe
Florian-code-s <florian.pexider@gmail.com>
2026-02-20 16:46:16 +0100

    Structure change

---
+++ src/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from .routes.health import health_bp
+from .routes.monitor import monitor_bp
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(health_bp)
+    app.register_blueprint(monitor_bp)
+    return app
+
+if __name__ == "__main__":
+    create_app().run(debug=True)
--- src/main.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-def main():
-    response = requests.get("https://api.github.com")
-    print("Status Code: ", response.status_code)
-
-if __name__ == "__main__":
-    main()
